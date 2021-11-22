c50dc81f78e0bb96d18c08a9983673760cb2cf80
xPi2 <uxiopineiro@gmail.com>
2021-11-22 17:38:18 +0000

    test: remove staker2 context & add second update end time tests

--- test/BottoLiquidityMiningV2.test.js
+++ test/BottoLiquidityMiningV2.test.js
@@ -46,7 +46,7 @@ contract("BottoLiquidityMiningV2", (accounts, network) => {
                 await time.increaseTo(start);
             });
 
-            context("staker1 stakes", function() {
+            context("stakers stakes", function() {
                 beforeEach(async function() {
                     await this.bottoEth.transfer(staker1, staker1Initial);
                     await this.bottoEth.approve(
@@ -55,11 +55,22 @@ contract("BottoLiquidityMiningV2", (accounts, network) => {
                         { from: staker1 }
                     );
                     tx1 = await this.miningProxy.stake(staker1Initial, { from: staker1 });
+                    /* Don't work with truffle test
+                    await this.bottoEth.transfer(staker2, staker2Initial);
+                    await this.bottoEth.approve(
+                        this.miningProxy.address,
+                        staker2Initial,
+                        { from: staker2 }
+                      );
+                    tx2 = await this.miningProxy.stake(staker2Initial, { from: staker2 });
+                    totalStakeForPeriod = staker1Initial.add(staker2Initial);
+                    */
+                    totalStakeForPeriod = staker1Initial;
                 });
 
                 it("has expected total stake", async function() {
                     expect(await this.miningProxy.totalStake()).to.be.bignumber.equal(
-                        staker1Initial
+                        totalStakeForPeriod
                     );
                 });
 
@@ -78,124 +89,137 @@ contract("BottoLiquidityMiningV2", (accounts, network) => {
                     );
                 });
 
-                context("staker2 stakes", function() {
-                    /*
-                    beforeEach(async function () {
-                      await this.bottoEth.transfer(staker2, staker2Initial);
-                      await this.bottoEth.approve(
-                        this.miningProxy.address,
-                        staker2Initial,
-                        { from: staker2 }
-                      );
-                      tx2 = await this.miningProxy.stake(staker2Initial, { from: staker2 });
+                context("advance to half stake time", function() {
+                    beforeEach(async function() {
+                        // totalStakeForPeriod = staker1Initial.add(staker2Initial);
+                        totalStakeForPeriod = staker1Initial;
+                        rewardPerSecond = totalRewards.div(stakingDuration);
+                        stakingHalfPeriod = stakingDuration.div(toBN("2"));
+                        await time.increaseTo(start.add(stakingHalfPeriod));
+                        reward1 = await this.miningProxy.payout.call({ from: staker1 });
+                        // reward2 = await this.miningProxy.payout.call({ from: staker2 });
                     });
-            
-                    it("has expected total stake", async function () {
-                      expect(await this.miningProxy.totalStake()).to.be.bignumber.equal(
-                      staker1Initial.add(staker2Initial)
-                      );
-                    });
-            
-                    it("has expected staker2 stake", async function () {
-                      expect(
-                        await this.miningProxy.totalUserStake(staker2)
-                      ).to.be.bignumber.equal(staker2Initial);
-                    });
-                    */
 
-                    context("advance to half stake time", function() {
+                    it("staker1 has expected rewards", async function() {
+                        expectedReward = stakingHalfPeriod.mul(rewardPerSecond).mul(staker1Initial).div(totalStakeForPeriod);
+                        expect(reward1).to.be.bignumber.closeTo(
+                            expectedReward, expectedReward.div(toBN("100000"))
+                        );
+                    });
+
+                    context("upgrade contract", function() {
                         beforeEach(async function() {
-                            // totalStakeForPeriod = staker1Initial.add(staker2Initial);
-                            totalStakeForPeriod = staker1Initial;
-                            rewardPerSecond = totalRewards.div(stakingDuration);
-                            stakingHalfPeriod = stakingDuration.div(toBN("2"));
-                            await time.increaseTo(start.add(stakingHalfPeriod));
-                            reward1 = await this.miningProxy.payout.call({ from: staker1 });
-                            // reward2 = await this.miningProxy.payout.call({ from: staker2 });
+                            miningProxy = await upgradeProxy(this.miningProxy.address, BottoLiquidityMiningV2);
                         });
 
-                        it("staker1 has expected rewards", async function() {
-                            expectedReward = stakingHalfPeriod.mul(rewardPerSecond).mul(staker1Initial).div(totalStakeForPeriod);
-                            expect(reward1).to.be.bignumber.closeTo(
-                                expectedReward, expectedReward.div(toBN("100000"))
-                            );
+                        it("staker1 has same rewards", async function() {
+                            newReward1 = await miningProxy.payout.call({ from: staker1 });
+                            expect(newReward1).to.be.bignumber.closeTo(reward1, reward1.div(toBN("100000")));
                         });
 
-                        context("upgrade contract", function() {
+                        context("extend stake time", function() {
                             beforeEach(async function() {
-                                miningProxy = await upgradeProxy(this.miningProxy.address, BottoLiquidityMiningV2);
+                                newEndTime = (await miningProxy.endTime()).add(time.duration.years(2));
+                                txEndTime = await miningProxy.updateEndTime(newEndTime);
                             });
 
-                            it("staker1 has same rewards", async function() {
+                            it("has expected first stake time", async function() {
+                                firstStakeTime = String(
+                                    (await web3.eth.getBlock(txEndTime.receipt.blockNumber)).timestamp
+                                );
+                                expect(await miningProxy.firstStakeTime()).to.be.bignumber.equal(
+                                    firstStakeTime
+                                );
+                            });
+
+                            it("has expected end time", async function() {
+                                expect(await miningProxy.endTime.call()).to.be.bignumber.equal(newEndTime);
+                            });
+
+                            it("has same rewards at the time of the extend", async function() {
                                 newReward1 = await miningProxy.payout.call({ from: staker1 });
                                 expect(newReward1).to.be.bignumber.closeTo(reward1, reward1.div(toBN("100000")));
                             });
 
-                            context("extend stake time", function() {
+                            it("has same total deposit rewards as the initial", async function() {
+                                let totalDepositRewards = await miningProxy.totalDepositRewards();
+                                expect(totalDepositRewards).to.be.bignumber.equal(totalRewards);
+                            })
+
+                            context("advance to half new stake time", async function() {
                                 beforeEach(async function() {
-                                    newEndTime = end.add(time.duration.years(2));
-                                    txEndTime = await miningProxy.updateEndTime(newEndTime);
+                                    previousRewards = await miningProxy.payout.call({ from: staker1 });
+                                    let firstStakeTime = await miningProxy.firstStakeTime();
+                                    let endTime = await miningProxy.endTime();
+                                    let totalRewards = await miningProxy.totalRewards();
+                                    newStakingDuration = endTime.sub(firstStakeTime);
+                                    rewardPerSecond = totalRewards.div(newStakingDuration);
+
+                                    await time.increase(newStakingDuration.div(toBN("2")));
                                 });
 
-                                it("has expected first stake time", async function() {
-                                    firstStakeTime = String(
-                                        (await web3.eth.getBlock(txEndTime.receipt.blockNumber)).timestamp
+                                it("has expected rewards", async function() {
+                                    let currentRewards = await miningProxy.payout.call({ from: staker1 });
+                                    expectedRewards = newStakingDuration
+                                        .div(toBN("2"))
+                                        .mul(rewardPerSecond)
+                                        .mul(staker1Initial)
+                                        .div(totalStakeForPeriod)
+                                        .add(previousRewards);
+                                    expect(currentRewards).to.be.bignumber.closeTo(
+                                        expectedRewards, expectedRewards.div(toBN("100000"))
                                     );
-                                    expect(await miningProxy.firstStakeTime()).to.be.bignumber.equal(
-                                        firstStakeTime
-                                    );
                                 });
 
-                                it("has expected end time", async function() {
-                                    expect(await this.miningProxy.endTime.call()).to.be.bignumber.equal(newEndTime);
-                                });
-
-                                it("staker1 has same rewards", async function() {
-                                    newReward1 = await this.miningProxy.payout.call({ from: staker1 });
-                                    expect(newReward1).to.be.bignumber.closeTo(reward1, reward1.div(toBN("100000")));
-                                });
-
-                                it("should have same total deposit rewards as the initial", async function() {
-                                    let totalDepositRewards = await miningProxy.totalDepositRewards();
-                                    expect(totalDepositRewards).to.be.bignumber.equal(totalRewards);
-                                })
-
-                                context("has expected rewards after extending the time", async function() {
+                                context("reduce stake time", function() {
                                     beforeEach(async function() {
-                                        reward1 = await this.miningProxy.payout.call({ from: staker1 });
-                                        firstStakeTime = await miningProxy.firstStakeTime();
-                                        let endTime = await miningProxy.endTime();
+                                        previousRewards = await miningProxy.payout.call({ from: staker1 });
+                                        let newEndTime = (await miningProxy.endTime()).sub(time.duration.years(0.5));
+
+                                        await miningProxy.updateEndTime(newEndTime);
+
                                         let totalRewards = await miningProxy.totalRewards();
-                                        newStakingDuration = endTime.sub(firstStakeTime)
-                                        rewardPerSecond = totalRewards.div(newStakingDuration)
+
+                                        firstStakeTime = await miningProxy.firstStakeTime();
+                                        newStakingDuration = (await miningProxy.endTime()).sub(firstStakeTime);
+                                        rewardPerSecond = totalRewards.div(newStakingDuration);
 
                                         await time.increase(newStakingDuration.div(toBN("2")));
                                     });
-                                    it("staker1 has expected rewards", async function() {
-                                        let currentRewards = await this.miningProxy.payout.call({ from: staker1 });
-                                        expectedReward = newStakingDuration
+
+                                    it("has expected rewards", async function() {
+                                        let currentRewards = await miningProxy.payout.call({ from: staker1 });
+                                        expectedRewards = newStakingDuration
                                             .div(toBN("2"))
                                             .mul(rewardPerSecond)
                                             .mul(staker1Initial)
                                             .div(totalStakeForPeriod)
-                                            .add(reward1)
+                                            .add(previousRewards);
                                         expect(currentRewards).to.be.bignumber.closeTo(
-                                            expectedReward, expectedReward.div(toBN("100000"))
+                                            expectedRewards, expectedRewards.div(toBN("100000"))
                                         );
                                     });
-                                });
-                                context("has expected rewards at endtime", async function() {
-                                    beforeEach(async function() {
-                                        let endTime = await miningProxy.endTime();
-                                        await time.increaseTo(endTime);
-                                    });
-                                    it("staker1 has expected rewards", async function() {
-                                        let { reward } = (await miningProxy.withdraw.call({ from: staker1 }));
 
-                                        expect(reward).to.be.bignumber.closeTo(
-                                            totalRewards, totalRewards.div(toBN("100000"))
-                                        )
-                                    });
+                                    it("has same total deposit rewards as the initial", async function() {
+                                        let totalDepositRewards = await miningProxy.totalDepositRewards();
+                                        expect(totalDepositRewards).to.be.bignumber.equal(totalRewards);
+                                    })
+
+                                });
+                            });
+
+                            context("advance to staking end time", async function() {
+                                beforeEach(async function() {
+                                    let endTime = await miningProxy.endTime();
+                                    await time.increaseTo(endTime);
+                                });
+
+                                it("staker1 has expected rewards", async function() {
+                                    let { reward } = (await miningProxy.withdraw.call({ from: staker1 }));
+
+                                    expect(reward).to.be.bignumber.closeTo(
+                                        totalRewards, totalRewards.div(toBN("100000"))
+                                    )
                                 });
                             });
                         });
